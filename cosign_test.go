641fac812ba4104a4b423a0b1dbece6acb3724d5
hackeramitkumar <amit9116260192@gmail.com>
2023-06-20 22:48:01 +0530

    added tests

--- cosign_test.go
+++ cosign_test.go
@@ -4,6 +4,14 @@ import (
 	"testing"
 )
 
+var (
+	image = "localhost:5001/demo-refer:app3"
+)
+
 func Test_Cosign2(t *testing.T) {
 	main()
+}
+
+func test_keyless(t *testing.T) {
+	keyless_sigantureVerification(t, ctx)
 }
