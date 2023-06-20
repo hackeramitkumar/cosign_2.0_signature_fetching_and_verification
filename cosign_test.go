61caf0430aa0660665c1b3d301f7c9d8b5d8197d
hackeramitkumar <amit9116260192@gmail.com>
2023-06-20 22:48:28 +0530

    added test for attestations

--- cosign_test.go
+++ cosign_test.go
@@ -8,10 +8,26 @@ var (
 	image = "localhost:5001/demo-refer:app3"
 )
 
-func Test_Cosign2(t *testing.T) {
+func test_Cosign2(t *testing.T) {
 	main()
 }
 
+func test_fetch_signatures_and_manifests(t *testing.T) {
+	cosign2(image)
+}
+
+func test_keyed(t *testing.T) {
+	keyed_signatureVerification(image)
+}
+
 func test_keyless(t *testing.T) {
-	keyless_sigantureVerification(t, ctx)
+	keyless_sigantureVerification(image)
+}
+
+func test_fetch_attached_artifacts_using_referrer_API(t *testing.T) {
+	fetch_attestations(image)
+}
+
+func test_verify_attestation(t *testing.T) {
+	verifyAttestaions(image)
 }
